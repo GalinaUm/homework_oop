0d9cc635a9c2f8c75dc9daf934d08f49686b58c3
GalinaUm <galinaumerzakhova@yandex.ru>
2025-10-02 17:32:12 +0300

    classes in new modules

--- src/14.1_main.py
+++ src/14.1_main.py
@@ -1,35 +1,5 @@
-
-
-class Product:
-    name: str
-    description: str
-    price: float
-    quantity: int
-
-    def __init__(self, name, description, price, quantity):
-        self.name = name
-        self.description = description
-        self.price = price
-        self.quantity = quantity
-
-
-
-class Category:
-    name: str
-    description: str
-    products: list
-    category_count = 0
-    product_count = 0
-
-    def __init__(self, name, description, products=None):
-        self.name = name
-        self.description = description
-        self.products = products if products else []
-        Category.category_count += 1
-        Category.products_count = len(products) if products else 0
-
-
-
+from src.Category import Category
+from src.Product import Product
 
 if __name__ == "__main__":
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
