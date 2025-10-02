a21a4abe3e520b58dfda7d6a9ab2899f68e2fbc3
GalinaUm <galinaumerzakhova@yandex.ru>
2025-10-02 20:49:23 +0300

    test_category

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,2 +1,11 @@
-def test_category_init(category):
+from src.category import Category
+from tests.conftest import first_product, second_product, third_product
+
+
+def test_category_init(category, first_product, second_product, third_product):
     assert category.name == "Смартфоны"
+    assert category.description == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+    assert category.products == [first_product, second_product, third_product]
+    assert Category.category_count == 1
+    assert Category.product_count == 3
+
