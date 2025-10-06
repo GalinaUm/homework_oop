c64902c581c8a1e36f741afc37bf38d75f687cab
GalinaUm <galinaumerzakhova@yandex.ru>
2025-10-06 20:00:05 +0300

    category products private

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 class Category:
     """Класс, относящий продукт к определенной категории"""
     name: str
@@ -9,6 +12,16 @@ class Category:
     def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
         Category.category_count += 1
         Category.product_count = len(products) if products else 0
+
+    def add_product(self, product):
+        self.__products.append(product)
+
+    @property
+    def products(self):
+        return f'{Product.name}, {Product.price} руб. Остаток: {Product.quantity} шт.'
+
+
+
