e57e256c00a7a962c1d4d383963f8974140092a4
GalinaUm <galinaumerzakhova@yandex.ru>
2025-10-15 12:42:57 +0300

    test_product ready

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,4 +1,4 @@
-from tests.conftest import third_product
+from tests.conftest import third_product, second_product
 
 
 def test_product_init(first_product, second_product, third_product, fourth_product):
@@ -26,3 +26,8 @@ def test_product_add_price(first_product, second_product, third_product):
     assert first_product + second_product == 2580000.0
     assert first_product + third_product == 1334000.0
     assert second_product + third_product == 2114000.0
+
+def test_product_str(first_product, second_product, third_product):
+    assert str(first_product) == "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт."
+    assert str(second_product) == "Iphone 15, 210000.0 руб. Остаток: 8 шт."
+    assert str(third_product) == "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт."
